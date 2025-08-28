45939e9c090b4746aff4cfd6104f45d27f49a10b
gaaramk <gaarakml@gmail.com>
2025-08-28 07:33:33 +0300

    Add folders

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,6 @@ import WhatDoYoThink from "../WhatDoYoThink/WhatDoYoThink"
 import LoaderPage from "../LoaderPage/LoaderPage"
 import { useQuery } from "@tanstack/react-query"
 import { getAllPosts } from "../../lib/api"
-import PullToRefresh from "react-simple-pull-to-refresh"
 
 const Home = () => {
 
@@ -26,19 +25,13 @@ const Home = () => {
     return (
 
         <>
-            <PullToRefresh
-                onRefresh={refetch}
-                pullDownContent={<h3 className="text-gray-500">⬇️ Pull to refresh</h3>}
-                releaseContent={<h3 className="text-gray-500">↕️ Release to refresh</h3>}
-                refreshContent={<LoaderPage />}
-            >
+
                 <section className="my-20">
                     <WhatDoYoThink />
                     {data?.data?.posts.map((post) => (
                         <Post key={post._id} post={post}  />
                     ))}
                 </section>
-            </PullToRefresh>
 
 
 
